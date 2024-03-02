811698288e834d7ddd9d8135e1a9b7cd4b6f910c
huryelsouto <huryel.souto@gmail.com>
2024-03-02 16:27:18 -0300

    finishing user page

--- scripts/usuario.js
+++ scripts/usuario.js
@@ -1,9 +1,12 @@
 function mostrarConteudo(conteudo) {
-    var conteudoDiv = document.getElementById('conteudo');
+    var conteudoDiv = document.getElementById("conteudo-container");
     conteudoDiv.innerHTML = "Conteúdo da página " + conteudo;
 }
 
 function toggleMenu() {
+    var menu_content = document.getElementById('menu-content');
+    menu_content.classList.toggle('expandido');
+
     var menu = document.getElementById('menu');
     menu.classList.toggle('expandido');
 }
